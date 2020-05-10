565c2311233f5cf231386bb0c79423f153ff13bc
anil shetty <adshetty@libertyglobal.com>
2020-05-10 11:04:33 -0700

    set stdout and stderr to line buffered mode
    Try to improve quality and consistency of logging.

--- notify_comp/source/NotifyComponent/ssp_main.c
+++ notify_comp/source/NotifyComponent/ssp_main.c
@@ -238,6 +238,11 @@ int main(int argc, char* argv[])
     extern ANSC_HANDLE bus_handle;
     char *subSys            = NULL;  
     DmErr_t    err;
+
+    // Buffer characters till newline for stdout and stderr
+    setlinebuf(stdout);
+    setlinebuf(stderr);
+
     #ifdef FEATURE_SUPPORT_RDKLOG
     RDK_LOGGER_INIT();
     #endif
